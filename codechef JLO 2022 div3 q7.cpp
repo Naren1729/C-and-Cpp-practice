a86bac3d2d07be65084ae7e76b466a125021de65
Naren sri sai <75826273+Naren1729@users.noreply.github.com>
2022-06-19 15:23:10 +0530

    Add files via upload

---
+++ codechef JLO 2022 div3 q7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long x,n;
+        cin>>x>>n;
+        long long ans = (x-1)*(2*n - x);
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
